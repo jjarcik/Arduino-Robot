b1a5356daf4146695746f8032764d15ef4bc7996
jjarcik <jarcik.jan@gmail.com>
2016-10-01 00:50:31 +0200

    First step to connect via johnny-five

--- WiFi/README.md
+++ WiFi/README.md
@@ -3,3 +3,8 @@
 http://wifinodebot.blogspot.cz/2016/02/blink-led-over-wifi-with-nodejs-johnny.html
 
 Skript který dokáže ovládat diodu pomocí WiFi
+
+<img src='./imgs/KYbW1.png'  width="400"/> 
+
+<img src='./imgs/xesp8266.png'  width="400"/> 
+
--- WiFi/src/index.js
+++ WiFi/src/index.js
@@ -1,9 +1,29 @@
 console.log("start")
 
+var VirtualSerialPort = require('udp-serial').SerialPort;
+var firmata = require('firmata');
 var five = require("johnny-five");
-var board = new five.Board();
+ 
+//create the udp serialport and specify the host and port to connect to
+var sp = new VirtualSerialPort({
+  host: '192.168.4.1',
+  type: 'udp4',
+  port: 1025
+});
 
-board.on("ready", function() {
-  var led = new five.Led(13);
-  led.blink(500);
+//use the serial port to send a command to a remote firmata(arduino) device
+var io = new firmata.Board(sp);
+io.once('ready', function(){
+    console.log('IO Ready');
+    io.isReady = true;
+
+    var board = new five.Board({io: io, repl: true});
+
+    board.on('ready', function(){
+        console.log('five ready');
+        //Full Johnny-Five support here:
+
+        var led = new five.Led(13);
+        led.blink(500);
+    });
 });
